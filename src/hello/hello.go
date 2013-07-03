d3111ac628a0fa6158b3e8fb82497b7a3a901d50
Oleg M Prozorov <pro.oleg@gmail.com>
2013-07-04 01:18:05 +0400

    fixes for go1

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,23 +1,20 @@
 package hello
 
 import (
-	"http"
-	"template"
+	"net/http"
+	tt "text/template"
 	"strings"
 	"bytes"
-	//"fmt"
-	//"strconv"
+	//	"fmt"
+
 	"sort"
 	"io/ioutil"
 	"regexp"
-	"os"
-
-	"json"
+	"encoding/json"
 	"appengine"
 	"appengine/user"
 	"appengine/datastore"
 )
-
 
 type WordCount struct {
 	Word  string
@@ -33,9 +30,11 @@ func (uw UserWord) Key() string { return uw.Email + ":" + uw.Word }
 
 type WordCountArray []WordCount
 
-func (p WordCountArray) Len() int           { return len(p) }
-func (p WordCountArray) Less(i, j int) bool { return p[i].Count > p[j].Count } //>desc <asc
-func (p WordCountArray) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p WordCountArray) Len() int { return len(p) }
+func (p WordCountArray) Less(i, j int) bool { return p[i].Count > p[j].Count }
+
+//>desc <asc
+func (p WordCountArray) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 
 type menuLink struct {
 	Name  string
@@ -63,7 +62,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func tokenize(w http.ResponseWriter, r *http.Request) {
 	filename, content, err := safeFormFile("somename", r)
 	if err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -72,7 +71,7 @@ func tokenize(w http.ResponseWriter, r *http.Request) {
 	wordsCountResult := toWordCountArray(words)
 	sort.Sort(wordsCountResult)
 
-	//rows := toRows(wordsCountResult)  
+	//rows := toRows(wordsCountResult)
 
 	data := map[string]interface{}{
 		"Filename": filename,
@@ -86,14 +85,14 @@ func inout(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 	var url string
-	var err os.Error
+	var err error
 	if u == nil {
 		url, err = user.LoginURL(c, "/")
 	} else {
 		url, err = user.LogoutURL(c, "/")
 	}
 	if err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Location", url)
@@ -109,14 +108,14 @@ func add(w http.ResponseWriter, r *http.Request) {
 		Word:  r.FormValue("word"),
 	}
 
-	k := datastore.NewKey("UserWord", uw.Key(), 0, nil)
+	k := datastore.NewKey(c, "UserWord", uw.Key(), 0, nil)
 	_, err := datastore.Put(c, k, &uw)
 
 	var m map[string]string
 	if err == nil {
 		m = map[string]string{"ok": "true"}
 	} else {
-		m = map[string]string{"ok": "false", "msg": err.String()}
+		m = map[string]string{"ok": "false", "msg": err.Error()}
 	}
 
 	b, _ := json.Marshal(m)
@@ -128,13 +127,13 @@ func add(w http.ResponseWriter, r *http.Request) {
 func words(w http.ResponseWriter, r *http.Request) {
 	words, err := userWords(r)
 	if err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	renderPage("/words", "words.html", words, r, w)
 }
 
-func userWords(r *http.Request) ([]UserWord, os.Error) {
+func userWords(r *http.Request) ([]UserWord, error) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 	words := make([]UserWord, 0)
@@ -185,7 +184,7 @@ func toWordCountArray(m map[string]int) WordCountArray {
 	return wordsCountResult
 }
 
-func safeFormFile(fieldName string, r *http.Request) (filename string, content string, err os.Error) {
+func safeFormFile(fieldName string, r *http.Request) (filename string, content string, err error) {
 	file, handler, fileErr := r.FormFile(fieldName)
 	if fileErr != nil {
 		return "", "", fileErr
@@ -196,6 +195,7 @@ func safeFormFile(fieldName string, r *http.Request) (filename string, content s
 	}
 	return handler.Filename, string(bytesContent), nil
 }
+
 /*
    data := map[string]interface{} {
     "Filename": "123",
@@ -204,17 +204,31 @@ func safeFormFile(fieldName string, r *http.Request) (filename string, content s
 
 */
 func renderPage(active string, page string, pageData interface{}, r *http.Request, wr http.ResponseWriter) {
-	menuData := []menuLink{makeMenuLink("home", "/", active),
+	menuData := []menuLink{
+		makeMenuLink("home", "/", active),
 		makeMenuLink("upload", "/upload", active),
 		makeMenuLink("paste", "/paste", active),
 		makeMenuLink("words", "/words", active),
 		makeMenuLink("login/logout", "/inout", active)}
 
-	menu, _ := renderToString("_menu.html", menuData)
-	content, _ := renderToString(page, pageData)
+	menu, e1 := renderToString("_menu.html", menuData)
+	content, e2 := renderToString(page, pageData)
+	words, e3 := userWords(r)
+	sidebar, e4 := renderToString("_sidebar.html", words)
 
-	words, _ := userWords(r)
-	sidebar, _ := renderToString("_sidebar.html", words)
+	if e1 != nil {
+		http.Error(wr, e1.Error(), http.StatusInternalServerError)
+	}
+	if e2 != nil {
+		http.Error(wr, e2.Error(), http.StatusInternalServerError)
+	}
+	if e3 != nil {
+		http.Error(wr, e3.Error(), http.StatusInternalServerError)
+	}
+	if e4 != nil {
+		http.Error(wr, e4.Error(), http.StatusInternalServerError)
+	}
+
 
 	renderTemplate(menu, content, sidebar, wr)
 }
@@ -236,12 +250,11 @@ func renderTemplate(menu string, content string, sidebar string, wr http.Respons
 	render("_template.html", siteStructure, wr)
 }
 
-func renderToString(name string, data interface{}) (rendered string, error os.Error) {
-	tmpl := template.New(nil)
-	tmpl.SetDelims("{{", "}}")
+func renderToString(name string, data interface{}) (rendered string, error error) {
+	tmpl := tt.New(name)
 
 	b := bytes.NewBuffer([]byte{})
-	err := tmpl.ParseFile(name)
+	_, err := tmpl.ParseFiles(name)
 	if err == nil {
 		err = tmpl.Execute(b, data)
 	}
@@ -250,14 +263,13 @@ func renderToString(name string, data interface{}) (rendered string, error os.Er
 
 //TODO merge with renderToString ???
 func render(name string, data interface{}, wr http.ResponseWriter) {
-	tmpl := template.New(nil)
-	tmpl.SetDelims("{{", "}}")
+	tmpl := tt.New(name)
 
-	err := tmpl.ParseFile(name)
+	_, err := tmpl.ParseFiles(name)
 	if err == nil {
 		err = tmpl.Execute(wr, data)
 	}
 	if err != nil {
-		http.Error(wr, err.String(), http.StatusInternalServerError)
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
 	}
 }
